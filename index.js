9acea24693459c331e1df1606693e007e3cc431a
Sanket Goyal <48354938+Sanketgoyal26@users.noreply.github.com>
2020-04-19 17:13:10 +0530

    Login and Signup page with their respective routes

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var express=require('express')
 var app=express()
 
+app.set('view engine','ejs')
 var port = process.env.port || 3000
+
+app.get('/', function(req, res)
+{
+    res.render('login')
+})
+app.get('/signup', function(req, res)
+{
+    res.render('signup')
+})
 app.listen(port , function(req, res)
 {
     console.log("Server is running at port "+port)
